34b80d6898e39f994ca901050f4535554fcf72c6
Rushe846 <ttv.roixer846@gmail.com>
2024-12-05 16:17:43 +0100

    bug spawn inicial

--- clyde.py
+++ clyde.py
@@ -3,4 +3,4 @@ from fantasma import Fantasma
 
 class Clyde(Fantasma):
     def __init__(self):
-        super().__init__(2, "arriba", 200, 140, 1, 0, 96, 16, 16, pyxel.COLOR_GRAY)
+        super().__init__(2, "derecha", 200, 140, 1, 0, 96, 16, 16, pyxel.COLOR_GRAY)
--- pinky.py
+++ pinky.py
@@ -3,4 +3,4 @@ from fantasma import Fantasma
 
 class Pinky(Fantasma):
     def __init__(self):
-        super().__init__(1.5, "derecha", 200, 140, 1, 0, 64, 16, 16, pyxel.COLOR_GRAY)
+        super().__init__(1.5, "arriba", 200, 140, 1, 0, 64, 16, 16, pyxel.COLOR_GRAY)
