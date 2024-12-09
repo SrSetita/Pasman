90e711941541cee9e6d8355a9391ec149119ccee
SrSetita <mariobergia2006@gmail.com>
2024-12-09 17:55:30 +0100

    cambio puntosmapa

--- pared.py
+++ pared.py
@@ -17,9 +17,9 @@ class Pared:
         return False
 
     def draw(self):
-        if vg.puntosmapa < 192:
+        if vg.puntosmapa < 193:
             pyxel.blt(self.posicionX, self.posicionY,0,0,0, self.ancho, self.alto, pyxel.COLOR_GRAY)
-        if vg.puntosmapa >= 192 and vg.puntosmapa< 388:
+        if vg.puntosmapa >= 193 and vg.puntosmapa< 389:
             pyxel.blt(self.posicionX, self.posicionY,0,21,21, self.ancho, self.alto, pyxel.COLOR_GRAY)
-        if vg.puntosmapa >= 388:
+        if vg.puntosmapa >= 389:
             pyxel.blt(self.posicionX, self.posicionY,0,0,21, self.ancho, self.alto, pyxel.COLOR_GRAY)
