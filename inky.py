19242cf4389aef1847e95be45d90ca9923d4fbfe
SrSetita <mariobergia2006@gmail.com>
2024-12-09 17:49:26 +0100

    menos listos

--- inky.py
+++ inky.py
@@ -3,4 +3,4 @@ from fantasma import Fantasma
 
 class Inky(Fantasma):
     def __init__(self):
-        super().__init__(0.5, "izquierda", 200, 140, 1, 0, 0, 16, 16, 100, 15, pyxel.COLOR_GRAY)
+        super().__init__(0.5, "izquierda", 200, 140, 1, 0, 0, 16, 16, 100, 30, pyxel.COLOR_GRAY)
